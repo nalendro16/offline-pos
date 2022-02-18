6e49f3ad1218d58dfbcac548b37205d11987cef0
nalendro16 <nalendro_agung@yahoo.com>
2022-02-18 21:41:51 +0700

    styled-components added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css'
 import logo from './logo.svg'
-import styled from 'styled-components'
+import styled, { ThemeProvider } from 'styled-components'
 
+const theme = {
+  primary: 'red',
+  secondary: 'blue',
+}
 const Container = styled.div`
   text-align: center;
 `
-
 const Head = styled.h1`
-  color: #535;
+  color: ${(props) => props.theme.primary};
 `
 const Brand = styled.img`
   height: 10vh;
@@ -29,18 +32,36 @@ const Wrapper = styled.div`
     background: orange;
   }
 `
+const AnotherButton = styled.button`
+  ${(props) => {
+    console.log(props)
+    switch (props.variant) {
+      case 'primary':
+        return `background:blue`
+      case 'success':
+        return `background:green`
+      default:
+        return `background:grey`
+    }
+  }}
+`
 
 function App() {
   return (
-    <Container>
-      <Brand src={logo} alt="react" />
-      <Head>Styled Component</Head>
-      <Button>Primarry</Button>
-      <Button primary>Primarry</Button>
-      <TomatoButton>Tomato</TomatoButton>
-      <Wrapper>Wrapper</Wrapper>
-      <Wrapper className="test">Test Wrapper</Wrapper>
-    </Container>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Brand src={logo} alt="react" />
+        <Head>Styled Component</Head>
+        <Button>Primarry</Button>
+        <Button primary>Primarry</Button>
+        <TomatoButton>Tomato</TomatoButton>
+        <Wrapper>Wrapper</Wrapper>
+        <Wrapper className="test">Test Wrapper</Wrapper>
+        <AnotherButton variant="primary">Primary</AnotherButton>
+        <AnotherButton variant="success">Success</AnotherButton>
+        <AnotherButton>Gray</AnotherButton>
+      </Container>
+    </ThemeProvider>
   )
 }
 
