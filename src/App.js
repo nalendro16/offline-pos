26ad97ba16b4a964d8d2617998818ee737cc0768
nalendro16 <nalendro_agung@yahoo.com>
2022-02-18 20:58:37 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import logo from './logo.svg'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  text-align: center;
+`
+
+const Head = styled.h1`
+  color: #535;
+`
+const Brand = styled.img`
+  height: 10vh;
+  margin-top: 2rem;
+`
+const Button = styled.button`
+  color: #fff;
+  padding: 0.5rem 1rem;
+  border: none;
+  margin: 0 1rem;
+  background: ${(props) => (props.primary ? 'blue' : 'green')};
+`
+const TomatoButton = styled(Button)`
+  background: tomato;
+`
+const Wrapper = styled.div`
+  background: yellow;
+  &.test {
+    background: orange;
+  }
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <Brand src={logo} alt="react" />
+      <Head>Styled Component</Head>
+      <Button>Primarry</Button>
+      <Button primary>Primarry</Button>
+      <TomatoButton>Tomato</TomatoButton>
+      <Wrapper>Wrapper</Wrapper>
+      <Wrapper className="test">Test Wrapper</Wrapper>
+    </Container>
+  )
 }
 
-export default App;
+export default App
