1b5454e2ffc4c091291ea23c3ac9541ce23ce548
nalendro16 <nalendro_agung@yahoo.com>
2022-03-10 21:56:16 +0700

    add menu list

---
+++ src/components/ListMenu.js
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+
+export default function ListMenu() {
+  const [menu] = useState(['*Favorite', 'Makanan', 'Minuman', 'Snack'])
+  return (
+    <ul>
+      <li></li>
+    </ul>
+  )
+}
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -2,16 +2,22 @@ import styled from 'styled-components'
 
 const Card = styled.div`
   width: 17%;
-  height: 15rem;
+  height: 10rem;
   cursor: pointer;
+  border-radius: 10px;
+  box-shadow: 0px 0px 8px -4px #7a7a7a;
 `
 const CardImg = styled.img`
-  width: 100%;
+  margin: 10px 10px;
+  width: 80%;
   height: 50%;
 `
 const NamePrice = styled.div`
+  padding: 0 10px;
   display: flex;
   justify-content: space-between;
+  flex-direction: column;
+  text-align: center;
 `
 
 export default function ProductCard({ id, name, img, price }) {
